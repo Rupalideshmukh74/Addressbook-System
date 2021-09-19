8e1139dd79f1ea59897b067c2ddb178ad22ae36c
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-09-19 13:56:55 +0530

    [Rupali]Feat:Added welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_System_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Addressbook system");
+        }
+    }
+}
